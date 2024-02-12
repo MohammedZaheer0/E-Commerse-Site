a07d937b591f63e12adf039ff3a72612b50f2b53
Mohammed Zaheer <mdzaheer78622@gmail.com>
2024-02-12 17:28:37 +0530

    initial

--- main.js
+++ main.js
@@ -48,47 +48,17 @@ let FormBtn = document.querySelector(".submit");
 
 
 
+    // let ValidUrls = ["about", "blog", "cart", "contact", "index","404", "Sblog", "shop", "Sproduct", "thankyou"];
 
-// let ValidUrls = ["about", "blog", "cart", "contact", "index", "page-not-found", "Sblog", "shop", "Sproduct", "thankyou"];
+    // let CurrentUrl = window.location.pathname.split('/').pop().toLowerCase();
+    
+    // if (!ValidUrls.includes(CurrentUrl)) {
+    //     console.log("Redirecting to 404.html");
+    //     window.location.href = "/404.html";
+    // }
+    
+    
 
-// let CurrentUrl = window.location.pathname.split('/').pop().toLowerCase();
-
-// if (!ValidUrls.includes(CurrentUrl)) {
-//     console.log("Redirecting to page-not-found.html");
-//     window.location.href = "/page-not-found.html";
-// }
-
-
-let ValidUrls = [
-  "http://127.0.0.1:5500/about.html",
-  "http://127.0.0.1:5500/blog.html",
-  "http://127.0.0.1:5500/cart.html",
-  "http://127.0.0.1:5500/contact.html",
-  "http://127.0.0.1:5500/Index.html",
-  // "http://127.0.0.1:5500/404.html", 
-  "http://127.0.0.1:5500/Sblog.html", 
-  "http://127.0.0.1:5500/shop.html", 
-  "http://127.0.0.1:5500/Sproduct.html", 
-  "http://127.0.0.1:5500/thankyou.html"
-];
-
-urlExists(ValidUrls);
-
-async function urlExists(urls) {
-  for (let i = 0; i < urls.length; i++) {
-    try {
-      let response = await fetch(urls[i], { method: 'HEAD' });
-
-      if (!response.ok) {
-        window.location.href = "/404.html";
-        break; // Exit the loop if a non-OK response is encountered
-      }
-    } catch (error) {
-      console.error('Error checking URL:', error);
-      // Handle errors as needed
-    }
-  }
-}
 
 
 
